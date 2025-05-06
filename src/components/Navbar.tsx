131c678015ff88340c7936beb238b1311776a0c0
Imam Hari <imamhari3001@gmail.com>
2025-05-06 12:18:45 +0700

    menambahkan translate

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,79 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { Text, Lang } from "@/utils/Text";
 import Image from "next/image";
+import Link from "next/link";
+import { IoIosArrowDown } from "react-icons/io";
+
 function Navbar() {
+  const [lang, setLang] = useState<Lang>("id");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const changeLang = (newLang: Lang) => {
+    setLang(newLang);
+    setShowDropdown(false);
+  }
+
   return (
-    <section>
-      <div>
-        <Image
-        src={"/logo.webp"}
-        alt="logo"
-        width={50}
-        height={50}
-        />
-      </div>
-      <div>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+    <section className="bg-slate-900 p-4">
+      <div className="flex justify-between items-center mx-[10vw] ">
+        <div className="flex gap-2 items-center">
+          <Image src={"/logo.webp"} alt="logo" width={50} height={50} />
+          <p className=" text-white">NUSANTARA BITES</p>
+        </div>
+        <div className="hidden md:flex items-center justify-between">
+          <div className="flex gap-7 text-md font-semibold text-white items-center">
+            <Link href="/" className="hover:text-amber-400">
+              <span>{Text.navbar.home[lang]}</span>
+            </Link>
+            <Link href="/" className="hover:text-amber-400">
+              <span>{Text.navbar.about[lang]}</span>
+            </Link>
+            <Link href="/" className="hover:text-amber-400">
+              <span>{Text.navbar.product[lang]}</span>
+            </Link>
+            <Link href="/" className="hover:text-amber-400">
+              <span>{Text.navbar.gallery[lang]}</span>
+            </Link>
+            <Link href="/" className="hover:text-amber-400">
+              <span>{Text.navbar.contact[lang]}</span>
+            </Link>
+            <div className="relative">
+              <button
+                onClick={() => setShowDropdown(!showDropdown)}
+                className="flex items-center gap-2 px-3 py-2 border rounded bg-slate-900 text-white hover:bg-slate-800 cursor-pointer"
+              >
+                <Image
+                  src={`/assets/${lang}.png`}
+                  width={20}
+                  height={20}
+                  alt="flag"
+                />
+                <span>{lang.toUpperCase()}</span>
+                <IoIosArrowDown className={`text-white ${showDropdown ? "rotate-180" : ""}`} />
+              </button>
+
+              {showDropdown && (
+                <div className="absolute right-0 mt-2 w-30 bg-white shadow-md border rounded z-10">
+                  <button
+                    onClick={() => changeLang("id")}
+                    className="flex items-center gap-2 px-3 py-2 hover:bg-gray-100 w-full text-black"
+                  >
+                    <Image src="/assets/id.png" width={20} height={20} alt="ID" />
+                    Indonesia
+                  </button>
+                  <button
+                    onClick={() => changeLang("en")}
+                    className="flex items-center gap-2 px-3 py-2 hover:bg-gray-100 w-full text-black"
+                  >
+                    <Image src="/assets/en.png" width={20} height={20} alt="EN" />
+                    English
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/utils/Text.ts
@@ -0,0 +1,26 @@
+export type Lang = "en" | "id";
+
+export const Text = {
+    navbar: {
+        home: {
+            id: "Beranda",
+            en: "Home",
+        },
+        about: {
+            id: "Tentang Kami",
+            en: "About Us",
+        },
+        product: {
+            id: "Produk & Layanan",
+            en: "Products & Services",
+        },
+        gallery: {
+            id: "Galeri",
+            en: "Gallery",
+        },
+        contact: {
+            id: "Kontak",
+            en: "Contact",
+        },
+    }
+}
