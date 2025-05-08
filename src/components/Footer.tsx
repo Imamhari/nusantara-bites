08bd1abe8417472461b01c9f4d60ead465c454c1
Imam Hari <imamhari3001@gmail.com>
2025-05-08 14:51:24 +0700

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <section className='absolute bg-black w-full bottom-0'>
+        <div className='flex flex-col lg:flex-row  items-center justify-between text-sm py-1 text-white  mx-auto max-w-[1200px]'>
+        <p>© 2025 · All Right Reserved </p>
+        <p>Designed by - Co. Founder : Imam Hari</p>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
